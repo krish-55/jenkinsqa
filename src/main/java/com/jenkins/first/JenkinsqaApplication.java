87f222e8775d0584f12631db981c8300bcd199aa
Krish <krishstylishstar@gmail.com>
2026-02-08 13:20:19 +0530

    all files availables

---
+++ src/main/java/com/jenkins/first/JenkinsqaApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsqaApplication.class, args);
+	}
+
+}
